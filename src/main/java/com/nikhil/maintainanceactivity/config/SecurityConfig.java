d04555da945dbf29b132d0ab36241d003970c5d7
Nikhil <vinayak.pp73@gmail.com>
2024-02-28 13:44:30 +0530

    added security and filter class

--- src/main/java/com/nikhil/maintainanceactivity/config/SecurityConfig.java
+++ src/main/java/com/nikhil/maintainanceactivity/config/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.nikhil.maintainanceactivity.config;
 
+import com.nikhil.maintainanceactivity.service.*;
+import com.nikhil.maintainanceactivity.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.*;
+import org.springframework.security.web.authentication.*;
+
+@Configuration
 public class SecurityConfig{
+    @Autowired
+    JwtUtil jwtUtil;
+    @Autowired
+    JwtFilter jwtFilter;
+    @Autowired
+    UserService userService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(auth->
+                auth.requestMatchers("/auth").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 }
---
+++ src/main/java/com/nikhil/maintainanceactivity/util/JwtFilter.java
@@ -0,0 +1,43 @@
+package com.nikhil.maintainanceactivity.util;
+
+import com.nikhil.maintainanceactivity.service.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.context.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.web.filter.*;
+
+import java.io.*;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtUtil jwtUtil;
+    @Autowired
+    UserService userService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            String jwt = extractJwtFromRequest(request);
+            if(jwt!=null&&jwtUtil.validateToken(jwt)) {
+                String userName = jwtUtil.extractUserName(jwt);
+                UserDetails userDetails = userService.loadByUserName(userName);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            filterChain.doFilter(request,response);
+    }
+
+    private String extractJwtFromRequest(HttpServletRequest request) {
+        // Extract JWT from the Authorization header
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
